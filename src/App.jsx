c2994ed06260c155a7c5dfa55fec8e5798c35dc5
omareldesoky00 <omar.eldesoky02@gmail.com>
2026-07-06 11:53:24 +0300

    Initial React Vite project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+
+    return (
+
+        <h1
+            style={{
+                color: "white",
+                textAlign: "center",
+                marginTop: "100px"
+            }}
+        >
+            Noor City Dashboard
+        </h1>
+
+    );
+
+}
+
+export default App;
